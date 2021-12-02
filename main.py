4e93a1de406c11d98c3cf64daf1a318c826f999c
Jafer Alhaboubi <habboubi@gmail.com>
2021-12-02 10:58:22 -0600

    connections,query, and close

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# python connection with MariaDB
+import mariadb
+import sys
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    try:
+        conn = mariadb.connect(
+            user="root",
+            password="root",
+            host="127.0.0.1",
+            port=3306,
+            database="classicmodels"
+
+        )
+        conn.autocommit = True
+
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+    else:
+        print("connection opened!")
+    # Get Cursor
+    cur = conn.cursor()
+
+    cur.execute(
+        "select * from customers where country = ? order by contactLastName", ("USA",)
+    )
+
+    for i in cur:
+        print(i)
+
+    conn.close()
+
+
+
